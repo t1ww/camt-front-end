f7f262bfb5f6d88f36e2c0e19d48a33e37b4793b
t1ww <nongtews@gmail.com>
2024-02-09 14:44:39 +0700

    first lab08 commit

---
+++ Front end/LAB08/Lab08_resource/JanTemp/januaryTemp.js
@@ -0,0 +1,11 @@
+// Include the nodejs File system into your program
+
+
+var january = "48,42,68\n";
+january += "48,42,69\n";
+january += "49,42,69\n";
+january += "49,42,61\n";
+january += "49,42,65\n";
+january += "49,42,62\n";
+january += "49,42,62\n";
+
