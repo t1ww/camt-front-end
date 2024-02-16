38d3f691c8ca9c9db3f97e48ecb7fc64ea137cb5
t1ww <95153968+t1ww@users.noreply.github.com>
2024-02-16 17:07:18 +0700

    finish lab9

--- Front end/LAB09/server.js
+++ Front end/LAB09/server.js
@@ -1,28 +1,50 @@
 // write code here
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
-const { createConnection } = require("mysql2");
-const conn = createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    database: "mysql",
-    password: "",
-});
+app.use(bodyParser.json());
+
+const mysql = require("mysql2");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(3000, function () {
-    console.log("server listening to port 3000");
-});
-// connect to html
-app.get("/", function (request, response) {
-    response.sendFile(__dirname + "/index.html");
+const database = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "book_management",
 });
 
-app.post("/books/add", function (req, res) {
-    // do someting here
-    let bname = req.body.bookName.id;
-    console.log(bname);
+// connect
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+// send html
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "public" + "index.html");
+});
+
+// inserting book
+app.post("/books/add", (req, res) => {
+    const { bookName } = req.body;
+    database.execute("INSERT INTO books (BookName) VALUES (?)", [bookName], (error, results) => {
+        if (error) { // error check
+            console.error("Error inserting into the database: ", error);
+            return res.status(500).send("Internal Server Error");
+        }
+        console.log("Inserted book into database, see result : ", results);
+        res.redirect("/");
+    });
+});
+
+// select book
+app.get("/books", (req, res) => {
+    database.query("SELECT * FROM books", (err, rows) => {
+        if (err) {
+            console.error("Error fetching books from the database: ", err);
+            return res.status(500).send("Internal Server Error");
+        }
+        console.log("Retrieved books from database : ", rows);
+        res.json(rows);
+    });
 });
