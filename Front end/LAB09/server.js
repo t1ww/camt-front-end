a971c339268e6dd0fc29b1f57fc5aa0b4914191d
t1ww <95153968+t1ww@users.noreply.github.com>
2024-02-16 15:34:11 +0700

    sql connection

--- Front end/LAB09/server.js
+++ Front end/LAB09/server.js
@@ -1,21 +1,28 @@
 // write code here
-const express = require('express');
-const app = express(); 
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const { createConnection } = require('mysql2');
-const databaseConnection = createConnection(config);
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+const { createConnection } = require("mysql2");
+const conn = createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    database: "mysql",
+    password: "",
+});
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("server listening to port 3000");
 });
 // connect to html
-app.get("/", function(request,response){
+app.get("/", function (request, response) {
     response.sendFile(__dirname + "/index.html");
-    document.body.bookName
 });
 
-app.post("/books/add", function (req,res) {
+app.post("/books/add", function (req, res) {
     // do someting here
+    let bname = req.body.bookName.id;
+    console.log(bname);
 });
