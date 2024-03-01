8c1c6710096e22d3e16a54ef517d04bfbfd527a8
t1ww <95153968+t1ww@users.noreply.github.com>
2024-03-01 17:33:35 +0700

    finish
    with add button

--- Front end/LAB11/LAB11_resources/app.js
+++ Front end/LAB11/LAB11_resources/app.js
@@ -16,7 +16,7 @@ app.listen("3000", () => {
 // student's code here // >
 app.get("/", async (req, res) => {
     try {
-        const items = await listItem.getTodoItemsName();
+        const items = await listItem.findAll();
 
         // Render the view with the provided data
         res.render("list", {
@@ -24,12 +24,6 @@ app.get("/", async (req, res) => {
             newListItems: items,
         });
 
-        //inserting new item
-        const newitem = {
-            name: "SE262!",
-        };
-        listItem.create(newitem);
-        
         // Define and fetch items asynchronously
         await listItem.defineInitialItems();
 
@@ -42,4 +36,41 @@ app.get("/", async (req, res) => {
     }
 });
 
+app.post("/delete", async (req, res) => {
+    try {
+        const deleteItemId = req.body.checkbox; // Assuming checkbox is the name of your checkbox input
+        console.log("deleting item with this id : " + deleteItemId);
+        // Use the provided listItem.delete method
+        const result = await listItem.delete(deleteItemId);
 
+        // Check if the deletion was successful
+        if (result) {
+            console.log("Item deleted successfully.");
+        } else {
+            console.log("Item not found or deletion unsuccessful.");
+        }
+
+        // Redirect back to the main page after deletion
+        res.redirect("/");
+    } catch (error) {
+        // Handle errors appropriately
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+app.post("/add", async (req, res) => {
+    try {
+        //inserting new item
+        const newitem = {
+            name: "SE262!",
+        };
+        await listItem.create(newitem); // <- remove here
+
+        // Redirect back to the main page after deletion
+        res.redirect("/");
+    } catch (error) {
+        // Handle errors appropriately
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
